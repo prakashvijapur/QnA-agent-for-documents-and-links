3f1d9882052960f3754b9013bce0fb3e26e5d058
Aditya Sharma <aditya.s@sigmoidanalytics.com>
2023-07-31 16:43:34 +0530

    updated api file to include key-upload success message

--- src/api.py
+++ src/api.py
@@ -50,7 +50,7 @@ def check_key(): ###### Check if OpenAI API key is present
 def validate_key():
     try:
         r=openai.Completion.create(model=models, prompt="t.",max_tokens=5)
-        #st.sidebar.success("API key validated")
+        st.sidebar.success("API key validated")
         return True
     except:
         st.sidebar.error("API key invalid, please change the key")
